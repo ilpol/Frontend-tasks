0be564baf253ba280c3faaaa5effc6b9f4616a13
ilpol <ilya-polozov@mail.ru>
2020-06-15 14:36:39 +0300

    rewritten with allSettled

--- Task28/script.js
+++ Task28/script.js
@@ -1,42 +1,53 @@
 
-function printMessage(text){
-  errorMessage = document.createElement("DIV");
-  errorMessage.innerText = text;
-  document.getElementById("wrap").appendChild(errorMessage);
+function printMessage(text) {
+	errorMessage = document.createElement("DIV");
+	errorMessage.innerText = text;
+	document.getElementById("wrap").appendChild(errorMessage);
+}
+
+async function toJson (res){
+	let jsonText= await res.json();
+	return jsonText;
 }
 
 async function fetchStart() {
-    const methodServiceAvailable = '/serviceavailable/';
-    let responseServiceAvailable = await fetch(methodServiceAvailable);
+	const methodServiceAvailable = '/serviceavailable/';
+	let responseServiceAvailable = await fetch(methodServiceAvailable);
 
 	if (responseServiceAvailable.ok) { 
-	  let jsonIs = await responseServiceAvailable.json();
-	  if (!jsonIs["isSucceeded"])
-	  	printMessage('Произошла ошибка');
-	  else {
-	  	let methodGetInfo = '/getinfo/';
-	  	let methodGetDescription = '/getdescription/';
-
-	  	let resGetInfo = await fetch(methodGetInfo);
-	  	let jsonGetInfo = await resGetInfo.json();
-
-	  	let resGetDescription = await fetch(methodGetDescription);
-	  	let jsonGetDescription = await resGetDescription.json();
-
-	  	if (resGetInfo.ok) {
-	  		printMessage(jsonGetInfo["text"]);
-	  	}
-	  	
-	  	if (resGetDescription.ok) {
-	  		printMessage(jsonGetDescription["text"]);
-	  	}
-
-	  	if ((!resGetInfo.ok && !resGetDescription.ok) || (!jsonGetInfo["isSucceeded"] && !jsonGetDescription["isSucceeded"]))
-	  		console.log("Server Error");  	
-	  }
-	  
-	} else {
-	  printMessage('Произошла ошибка');
+		let jsonIs = await responseServiceAvailable.json();
+		if (!jsonIs["isSucceeded"])
+			printMessage('Произошла ошибка');
+		else {
+			let methodGetInfo = '/getinfo/';
+			let methodGetDescription = '/getdescription/';
+			let successful = false;
+			
+			Promise.allSettled([fetch(methodGetInfo),fetch(methodGetDescription)]).then(results => {	    
+			toJsonList = []
+			results.forEach((result, num) => {
+				if (result.status == "fulfilled") {		    		
+					toJsonList.push(toJson(result['value']))           			    	      	
+				}		       
+			})
+            
+			Promise.allSettled(toJsonList).then(resultsJson => {
+				resultsJson.forEach((result,num) => {              	
+					if (result.status == "fulfilled") {
+						if (result['value']["isSucceeded"]) {
+							printMessage(result['value']["text"]);
+							successful = true;
+						}
+					}
+					else
+						console.log("Can not parse json");
+				})
+			}).then(() => {
+           		if (!successful)
+           			console.log("Server error");
+            })
+	        })	  
+		} 
 	}
-};
+}
 
